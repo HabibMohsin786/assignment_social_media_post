57fca5f9adde9a4e72043c3e36c46cbb51dd7632
Habib Mohsin <HabibMohsin034@gmail.com>
2024-06-01 15:18:30 +0500

    Almost compelete assignment

--- app.js
+++ app.js
@@ -33,14 +33,15 @@ const db = getDatabase();
 var inpProfileUrl = document.getElementById('inpProfileUrl');
 var inpProfileName = document.getElementById('inpProfileName');
 var inpPostUrl = document.getElementById('inpPostUrl');
+var perantPostCard = document.getElementById('perantPostCard');
 
-var allPosts;
+var allPostsData;
 
 window.addPostData = function () {
     var obj = {
         profileURL: inpProfileUrl.value,
         profileName: inpProfileName.value,
-        inpPostUrl: inpPostUrl.value,
+        postUrl: inpPostUrl.value,
     };
     var referance = ref(db, `Post/`);
     push(referance, obj);
@@ -49,8 +50,64 @@ window.addPostData = function () {
 function getData() {
     const referance = ref(db, `Post/`);
     onValue(referance, function (allposts) {
-        allPosts = allposts.val();
-        console.log(allPosts);
+        allPostsData = allposts.val();
+        // console.log(allPostsData);
+     var arr = Object.values(allPostsData);
+     console.log(arr);
+
+     perantPostCard.innerHTML = '';
+     for (let i = 0; i < arr.length; i++) {
+        perantPostCard.innerHTML += `<div class="m-0 p-0">
+
+        <nav class="navbar navbar-expand-lg bg-body-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img class="rounded-circle" width="60px" height="60px"
+                        src="${arr[i].profileURL}"
+                        alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                        <il class="nav-item">
+                            <a class="nav-link fw-bold" aria-current="page" href="#">${arr[i].profileName}</a>
+                            </li>
+                            <il class="nav-item">
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                                    <li class="nav-item">
+                                        <a class="nav-link fs-6" href="#">2 hours ago.</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link fs-6" href="#"><i class="fa-solid fa-user-group"></i></a>
+                                    </li>
+                                </ul>
+                            </il>
+
+
+                    </ul>
+                    <form class="d-flex " role="search">
+
+                        <button class="btn btn me-3 fs-3" type="submit"><i
+                                class="fa-solid fa-ellipsis"></i></button>
+                        <button class="btn fs-3" type="submit"><i class="fa-solid fa-xmark"></i></button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="postImage border border-1 rounded-2 m-2">
+        <img height="100%" width="100%"
+            src="${arr[i].postUrl}"
+            alt="">
+    </div>
+    <div class="d-flex justify-content-around my-2">
+        <button type="button" class="btn btn-light fw-bold"><i class="fa-solid fa-thumbs-up"></i><span class="ms-2">Like</span></button><button type="button"
+            class="btn btn-light fw-bold"><i class="fa-regular fa-comment"></i><span class="ms-2">Comment</span></button><button type="button" class="btn btn-light fw-bold"><i class="fa-solid fa-share"></i><span class="ms-2">Shear</span></button>
+    </div>`
         
+     }   
     })
 }
+getData();
